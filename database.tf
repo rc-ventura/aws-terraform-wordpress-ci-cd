034ba1b0bbfb2bac09675d287d8a99f6cedce88e
rc-ventura <r.c_ventura@hotmail.com>
2023-10-22 12:30:37 -0300

    Fix: rds and security groups

--- database.tf
+++ database.tf
@@ -1,56 +1,32 @@
-# RDS wordpress
-#----------------------------------------
-variable "db_allocated_storage" {
-  type        = number
-  description = "db allocated storage"
-  default     = 20
+# Create RDS
+#----------------------------------------------------
+
+resource "aws_db_instance" "wordpress" {
+  allocated_storage         = var.db_allocated_storage
+  max_allocated_storage     = var.db_max_allocated_storage
+  db_name                   = var.db_name
+  engine                    = var.db_engine
+  engine_version            = var.db_engine_version
+  instance_class            = var.db_instance_class
+  username                  = var.db_username
+  password                  = var.db_password
+  availability_zone         = local.azs[0]
+  publicly_accessible       = var.db_publicly_accessible
+  deletion_protection       = var.db_deletion_protection
+  db_subnet_group_name      = aws_db_subnet_group.wordpress.name
+  vpc_security_group_ids    = [aws_security_group.wordpress_rds.id]
+  skip_final_snapshot       = var.db_skip_final_snapshot
+
+  tags = {
+    "Name" = "${local.name_prefix}-${var.db_name}-db"
+  }
 }
-variable "db_max_allocated_storage" {
-  type        = number
-  description = "wordpress db max allocated storage"
-  default     = 500
-}
-variable "db_name" {
-  type        = string
-  description = "wordpress db name"
-}
-variable "db_instance_class" {
-  type        = string
-  description = "wordpress db instance type"
-  default     = "db.t2.micro"
-}
-variable "db_username" {
-  type        = string
-  description = "wordpress db username"
-  sensitive   = true
-}
-variable "db_password" {
-  type        = string
-  description = "wordpress db password"
-  sensitive   = true
-}
-variable "db_engine" {
-  type        = string
-  description = "wordpress db engine"
-  default     = "mysql"
-}
-variable "db_engine_version" {
-  type        = string
-  description = "wordpress db engine version"
-  default     = "8.0.32"
-}
-variable "db_publicly_accessible" {
-  type        = bool
-  description = "wordpress db publicly accessible"
-  default     = false
-}
-variable "db_deletion_protection" {
-  type        = bool
-  description = "wordpress db deletion protection"
-  default     = false
-}
-variable "db_skip_final_snapshot" {
-  type        = bool
-  description = "skip final snapshot"
-  default     = true
+
+resource "aws_db_subnet_group" "wordpress" {
+  name       = "${var.app_name}-db-subnet-group"
+  subnet_ids = module.network.private_subnets
+
+  tags = {
+    Name = "${local.name_prefix}-db-subnet-group"
+  }
 }
