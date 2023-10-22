decc94eef6dd79391bd58cf2cc7091db630a9232
rc-ventura <r.c_ventura@hotmail.com>
2023-10-22 16:02:14 -0300

    Create Auto Scaling and load Balancer

---
+++ auto_scaling.tf
@@ -0,0 +1,71 @@
+
+# Get amazon linux 2 ami
+data "aws_ami" "amazon_linux2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# Autoscaling group
+#----------------------------------------
+module "wordpress_asg" {
+  depends_on = [aws_db_instance.wordpress]
+  source     = "terraform-aws-modules/autoscaling/aws"
+  version    = "6.9.0"
+
+  name                = "${local.name_prefix}-${var.wordpress_server_name}-asg"
+  min_size            = var.wordpress_asg_min_size
+  max_size            = var.wordpress_asg_max_size
+  health_check_type   = var.wordpress_asg_health_check_type
+  vpc_zone_identifier = module.network.public_subnets
+  target_group_arns   = [aws_lb_target_group.web_alb_tg_http.arn]
+
+
+   # Launch template
+  launch_template_name        = "${var.wordpress_server_name}-template"
+  launch_template_description = "WordPress instances"
+
+  image_id          = data.aws_ami.amazon_linux2.id
+  instance_type     = var.wordpress_template_instance_type
+  enable_monitoring = var.wordpress_template_enable_monitoring
+  security_groups   = [aws_security_group.ssh_sg.id, aws_security_group.wordpress_ec2_sg.id]
+  key_name          = var.key_name
+
+  user_data = base64encode(data.template_file.user_data.rendered)
+
+  scaling_policies = {
+    avg-cpu-policy-greater-than-75 = {
+      policy_type = var.wordpress_asg_scaling_policy_type
+      target_tracking_configuration = {
+        predefined_metric_specification = {
+          predefined_metric_type = var.wordpress_asg_scaling_policy_metric_type
+        }
+        target_value = var.wordpress_asg_scaling_policy_target_value
+      }
+    }
+  }
+}
+
+
+# user data wordpress script template
+data "template_file" "user_data" {
+  template = file("${path.module}/install_wordpress.tpl")
+  vars = {
+    db_username = var.db_username
+    db_password = var.db_password
+    db_name     = var.db_name
+    db_host     = aws_db_instance.wordpress.address
+
+    wp_username = var.wp_username
+    wp_password = var.wp_password
+    wp_email    = var.wp_email
+    wp_url      = aws_lb.web_alb.dns_name
+    wp_title    = var.app_name
+  }
+}
+
+
---
+++ install_wordpress.tpl
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#template vars
+db_username=${db_username}
+db_password=${db_password}
+db_name=${db_name}
+db_host=${db_host}
+
+wp_username=${wp_username}
+wp_password=${wp_password}
+wp_email=${wp_email}
+wp_url=${wp_url}
+wp_title=${wp_title}
+
+sudo yum update -y
+sudo yum install -y httpd mysql
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+sudo amazon-linux-extras install -y php7.4
+sudo amazon-linux-extras enable php7.4
+sudo yum clean metadata
+sudo yum install php-cli php-pdo php-fpm php-json php-mysqlnd
+
+#Change OWNER and permission of directory /var/www
+sudo usermod -a -G apache ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo find /var/www -type d -exec chmod 2775 {} \;
+sudo find /var/www -type f -exec chmod 0664 {} \;
+
+# Download and install the WordPress CLI
+sudo curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+sudo chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
+
+#Create the WordPress configuration file
+cd /var/www/html
+wp core download --allow-root
+wp config create --dbname=$db_name --dbuser=$db_user --dbpass=$db_password --dbhost=$db_host --allow-root
+
+# Install WordPress
+wp core install --url=$wp_url --title=$wp_title --admin_user=$wp_username --admin_password=$wp_password --admin_email=$wp_email --allow-root
+
+# Set the appropriate permissions
+sudo chown -R apache:apache /var/www/html
+sudo chmod -R 755 /var/www/html
+sudo systemctl restart httpd.service
